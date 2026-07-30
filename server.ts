d39f9eb7bf09d8c8725afa75980d918df4135948
pqhacker-design <pqhacker@gmail.com>
2026-07-30 09:05:19 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { createServer as createViteServer } from "vite";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Body parser
 app.use(express.json({ limit: "50mb" }));
